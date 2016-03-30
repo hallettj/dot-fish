2f91d06ad22e67ada394c13b00dcb4c155eb2b1e
Jesse Hallett <jesse@sitr.us>
2016-03-30 16:28:53 -0700

    Change `lg` from abbr to alias

--- home/.config/fish/config.d/80-git-abbrs.fish
+++ home/.config/fish/config.d/80-git-abbrs.fish
@@ -1,7 +1,8 @@
 abbr -a add='git add -p'
 abbr -a commit='git commit'
-abbr -a lg="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 abbr -a pull='git pull'
 abbr -a push='git push'
 abbr -a show='git show'
 abbr -a st='git status'
+
+alias lg "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
