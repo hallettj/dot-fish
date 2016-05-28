05c72043631499b179acc555d6c260714ea26b34
Jesse Hallett <(none)>
2016-05-28 14:44:56 -0700

    Remove manual MANPATH and INFOPATH settings - the nix script does this better

--- home/.config/fish/config.d/40-nix.fish
+++ home/.config/fish/config.d/40-nix.fish
@@ -6,7 +6,5 @@ if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]
 end
 
 if [ -n "$NIX_LINK" ]
-  set -gx MANPATH "$NIX_LINK/share/man" $MANPATH
-  set -gx INFOPATH "$NIX_LINK/share/info" $INFOPATH
   set -gx CMAKE_PREFIX_PATH "$NIX_LINK"
 end
